6805098f871fd0e0132f3c619e50d7bdf65186c9
NotNathan <104657171+NotNathan24@users.noreply.github.com>
2023-02-14 00:19:25 +0000

    Update index.js

--- index.js
+++ index.js
@@ -23,26 +23,7 @@ const client = new Client({intents: [
 const commands = [];
 client.commands = new Collection();
 
-const helpEmbed = new EmbedBuilder()
-  .setColor(16706535)
-    .setTitle('Help Menu')
-    .addFields(
-    { name: "Here's a list of things I can do: ", value: "List of commands:"},
-        { name: '?help', value: 'Shows you this menu.'},
-        { name: '?play', value: 'Play a song!'},
-        { name: '?pause', value: 'Pause the music!'},
-        { name: '?skip', value: 'Skip the current song!'},
-        { name: '?queue', value: 'Shows the queue!'},
-        { name: '?resume', value: 'Resume the music!'},
-        { name: '?exit', value: 'Leave the voice channel!'},
-    )
-    .setThumbnail('https://i.imgur.com/xLbEPdp.jpeg ') 
-    .setTimestamp()
-    .setFooter({ text: 'Made by NotNathan24'});
-
-    const prefix="?"
-
-const commandsPath = path.join(__dirname, "commands"); // C:\Discord Music Bot\commands
+const commandsPath = path.join(__dirname, "commands"); // D:\NathanBot\NathanBot js\commands
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 for(const file of commandFiles)
 {
@@ -83,7 +64,7 @@ client.on("interactionCreate", async interaction => {
     if(!interaction.isCommand()) return;
 
     const command = client.commands.get(interaction.commandName);
-    if(!command) return;
+if(!command) return;
 
     try
     {
@@ -95,18 +76,5 @@ client.on("interactionCreate", async interaction => {
         await interaction.reply({content: "There was an error executing this command"});
     }
 });
-
-client.on("messageCreate", message => {
-    if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  const commandBody = message.content.slice(prefix.length);
-  const args = commandBody.split(' ');
-  const command = args.shift().toLowerCase();
-
-  if (command == "help") {
-    message.reply({ embeds: [helpEmbed] });
-  }
-})
 
 client.login(process.env.TOKEN);
